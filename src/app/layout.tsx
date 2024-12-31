9c1e5556e26a208d7dceff7c04c1693e39590f3c
ismail <omaranajar@gmail.com>
2024-12-31 01:59:35 +0100

    feat: add ai agent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 // import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -44,6 +45,33 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
+        <Script id="agancy" strategy="afterInteractive">
+          {`
+             (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '676fe1e7cbabbcd8e3fc3a56' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production'
+        }).then(() => {
+          window.voiceflow.chat.proactive.clear(); // clear all previous messages
+        setTimeout(function () {
+    window.voiceflow.chat.open();
+  }, 1000)
+          window.voiceflow.chat.proactive.push({ 
+            type: 'text', 
+            payload: { message: 'Are you interested in some 🔥🔥🔥 sneakers?' }
+          }, { 
+            type: 'text', 
+            payload: { message: 'Click on the chat to learn more!' }
+          });
+        });;
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+            `}
+        </Script>
       </body>
     </html>
   );
